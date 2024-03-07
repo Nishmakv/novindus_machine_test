9988d20a535726135cd96324120852394a7da661
Nishmakv <nishmakv3@gmail.com>
2024-03-07 17:05:00 +0530

    Finish project initialization
    1. Added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:novindus_machine_test/presentation/pages/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
+      theme: ThemeData(
+       
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
       home: HomeScreen(),
     );
   }
